4fea878184bf922da9b8638dbfb52ab1b3711698
Ggaabriel <djskilleton@gmail.com>
2026-09-08 23:43:08 +0600

    first test

---
+++ server/src/smoke/database.smoke.spec.ts
@@ -0,0 +1,24 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from '../prisma/prisma.service';
+
+describe('Smoke Database test', () => {
+  let prisma: PrismaService;
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [PrismaService],
+    }).compile();
+
+    prisma = module.get(PrismaService);
+
+    await prisma.$connect();
+  });
+
+  afterAll(async () => {
+    await prisma.$disconnect();
+  });
+
+  it('can we connect to database', async () => {
+    await expect(prisma.$queryRaw`SELECT 1`).resolves.toBeDefined();
+  });
+});
